fac2b5172ca743f30434caaf79aa82b640fa73c4
Andrew Nguyen <388693+andrewnguyenism@users.noreply.github.com>
2020-12-27 23:20:03 +1100

    chore: update changelog and version

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const Footer = () => (
   <div className="text-center text-xs py-4 mb-4">
-    <Link href="/changelog"><a className="block mb-2 underline">v1</a></Link>
+    <Link href="/changelog"><a className="block mb-2 underline">v2</a></Link>
     <p>Created by Andrew Nguyen in 2020.</p>
   </div>
 )
--- pages/changelog.js
+++ pages/changelog.js
@@ -12,6 +12,19 @@ export default function Changelog() {
         <div className="text-2xl mt-4 text-center uppercase mb-4">Changelog</div>
         <div className="rounded border-2 m-4 p-4">
           <div className="text-xl font-bold">
+            Version 2
+          </div>
+          <div className="text-sm">
+            28/12/2020
+          </div>
+          <ul className="list-disc ml-4 mt-4">
+            <li>
+              Fixed Join Game functionality on homepage.
+            </li>
+          </ul>
+        </div>
+        <div className="rounded border-2 m-4 p-4">
+          <div className="text-xl font-bold">
             Version 1
           </div>
           <div className="text-sm">
