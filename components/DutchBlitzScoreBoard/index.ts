310a6f4c1c250c60adc956e39d8252310676fe4c
Andrew Nguyen <388693+andrewnguyenism@users.noreply.github.com>
2021-01-02 15:01:09 +1100

    fix: remove export type from DutchBlitzScoreBoard

--- components/DutchBlitzScoreBoard/index.ts
+++ components/DutchBlitzScoreBoard/index.ts
@@ -1,4 +1,3 @@
 export { DutchBlitzScoreBoard } from './DutchBlitzScoreBoard'
 export { DutchBlitzScoreBoardHeader } from './DutchBlitzScoreBoardHeader'
 export { DutchBlitzScoreBoardPlayer } from './DutchBlitzScoreBoardPlayer'
-export type { DutchBlitzPlayer } from './DutchBlitzScoreBoardPlayer'
